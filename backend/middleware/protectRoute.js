ffb39490b5cda01c200d1c68edef9d82b6a1f18a
tylujianan <ljn199379@icloud.com>
2025-04-15 19:41:13 +0800

    delete node_modules

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,28 +1,33 @@
-import jwt from 'jsonwebtoken';
-import User from '../models/user.model.js';
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
 const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.jwt;
+
         if (!token) {
             return res.status(401).json({ error: "Unauthorized - No Token Provided" });
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify the token using the secret key from environment variables
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
         if (!decoded) {
-            return res.status(401).json({ error: "Unauthorized - Invalid Token" }); // Return an error if the token is invalid
-        }
-        const user = await User.findById(decoded.userId).select("-password"); // Find the user in the database using the user ID from the token payload
-        if (!user) {
-            return res.status(401).json({ error: "Unauthorized - User Not Found" }); // Return an error if the user is not found in the database 
+            return res.status(401).json({ error: "Unauthorized - Invalid Token" });
         }
 
-        req.user = user; // Attach the user object to the request object for further use in the route handler
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        req.user = user;
+
         next();
     } catch (error) {
-        console.error(error); // Log the error for debugging purposes
-        res.status(500).json({ message: error.message });
+        console.log("Error in protectRoute middleware: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
     }
-}
+};
 
-export default protectRoute; // Export the protectRoute function as the default export of this module
+export default protectRoute;
