2ce8255ed6ce2443271b4b3892810cca1655494d
tylujianan <ljn199379@icloud.com>
2025-04-19 14:41:03 +0800

    优化sidebar获取users时loading状态

--- frontend/src/components/Sidebar/Conversations.jsx
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -5,16 +5,20 @@ import { getRandomEmoji } from "../../utils/emojis";
 const Conversations = () => {
   const { isloading, conversations } = useGetConversations();
   return (
-    <div>
-      {conversations.map((conversation, idx) => (
-        <Conversation
-          key={conversation._id}
-          conversation={conversation}
-          emoji={getRandomEmoji()}
-          lastIdx={idx === conversations.length - 1}
-        />
-      ))}
-      {isloading ? <span className='loading loading-spinner mx-auto'></span> : null}
+    <div className="flex justify-center items-center h-full">
+      {
+        !isloading && <div className="h-full w-full">
+          {conversations.map((conversation, idx) => (
+            <Conversation
+              key={conversation._id}
+              conversation={conversation}
+              emoji={getRandomEmoji()}
+              lastIdx={idx === conversations.length - 1}
+            />
+          ))}
+        </div>
+      }
+      {isloading ? <span className='loading loading-bars text-info'></span> : null}
     </div>
   );
 };
